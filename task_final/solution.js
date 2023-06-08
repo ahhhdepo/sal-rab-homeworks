abb025652bfe9978ba1f8073749ab98cae352155
Kseniia <ksusha_2002@inbox.ru>
2023-06-08 09:14:44 +0700

    task final fix

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,20 +34,17 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
-
+    let data =  {client: `${name} ${phone}`, order: {}, goods: [] };
     let countOfGoods = goods.length;
-
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    let result = {data}
+    for (let i = 0; i < countOfGoods; i += 1) {
+        result.data.goods.push({title: goods[i].title, count: goods[i].count});
     }
+    let straddres =`ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв. ${address.flat}`
+    data.order.address = straddres;
+    data.order.sum = sum;
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
+    let jsonData = JSON.stringify(result)
 
     return jsonData;
 }
